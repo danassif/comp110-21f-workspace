39339511b23173092165cfa4647ff6b7ca87ddea
David Nassif <danassif@live.unc.edu>
2021-11-23 14:22:00 -0500

    updates

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -2,4 +2,81 @@
 
 __author__ = "123456789"
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(path: str) -> list[dict[str, str]]:
+    """Read a CSV file and return a list of its rows."""
+    file_handle = open(path, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    rows: list[dict[str, str]] = []
+    for row in csv_reader:
+        rows.append(row)
+    file_handle.close()
+    return rows
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Return a column's values."""
+    values: list[str] = []
+    for row in table:
+        values.append(row[column])
+    return values
+
+
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Convert a table of rows to a table of columns."""
+    result: dict[str, list[str]] = {}
+    for key in table[0]:
+        result[key] = column_values(table, key)
+    return result
+
+
+def head(table: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """done."""
+    result: dict[str, list[str]] = {}
+    if N < len(table):
+        for col in table:
+            i: int = 0
+            empty_list: list[str] = []
+            while i < N:
+                empty_list.append(table[col][i])
+                i += 1
+            result[col] = empty_list
+        return result
+    else:
+        return table
+
+
+def select(table: dict[str, list[str]], cols: list[str]) -> dict[str, list[str]]:
+    """Select only a subset of columns."""
+    result: dict[str, list[str]] = {}
+    for col in cols:
+        result[col] = table[col]
+    return result
+
+
+def concat(table1: dict[str, list[str]], table2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Concat two dicts."""
+    result: dict[str, list[str]] = {}
+    for col in table1:
+        result[col] = table1[col]
+    
+    for col in table2:
+        if col in result:
+            result[col] += table2[col]
+        else:
+            result[col] = table2[col]
+
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Get counts of each value."""
+    counts: dict[str, int] = {}
+    for value in values:
+        if value in counts:
+            counts[value] += 1
+        else:
+            counts[value] = 1
+    return counts
--- exercises/ex08/Simpy.py
+++ exercises/ex08/Simpy.py
@@ -8,6 +8,119 @@ __author__ = "YOUR PID HERE"
 
 
 class Simpy:
+    """Simpy."""
     values: list[float]
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float] = []):
+        """Constructor."""
+        self.values = values
+
+    def __str__(self) -> str:
+        """Repr."""
+        return f"Simpy({self.values})"
+
+    def fill(self, value: float, n: int = -1) -> None:
+        """Fill."""
+        xs: list[float] = []
+        for _ in range(n):
+            xs.append(value)
+        self.values = xs
+
+    def arange(self, start: float, stop: float, step: float = 1.0) -> None:
+        """Arrange."""
+        xs: list[float] = []
+        if step > 0:
+            while start < stop:
+                xs.append(start)
+                start += step
+        elif step < 0:
+            while start > stop:
+                xs.append(start)
+                start += step
+        else:
+            raise ValueError("Step cannot be 0.0.")
+        self.values = xs
+
+    def sum(self) -> float:
+        """Sum."""
+        return sum(self.values)
+
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Add."""
+        xs: list[float] = []
+        if isinstance(rhs, Simpy):
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                xs.append(self.values[i] + rhs.values[i])
+        else:
+            for item in self.values:
+                xs.append(item + rhs)
+        return Simpy(xs)
+
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Pow."""
+        xs: list[float] = []
+        if isinstance(rhs, Simpy):    
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                xs.append(self.values[i] ** rhs.values[i])
+        else:
+            for item in self.values:
+                xs.append(item ** rhs)
+        return Simpy(xs)
+
+    def __mod__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Mod."""
+        xs: list[float] = []
+        if isinstance(rhs, Simpy):    
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                xs.append(self.values[i] % rhs.values[i])
+        else:
+            for item in self.values:
+                xs.append(item % rhs)
+        return Simpy(xs)
+        
+    def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Eq."""
+        xs: list[bool] = []
+        if isinstance(rhs, Simpy):
+            assert len(self.values) == len(rhs.values)
+            for i in range(len(self.values)):
+                xs.append(self.values[i] == rhs.values[i])
+        else:
+            for item in self.values:
+                xs.append(item == rhs)
+        return xs
+
+    def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Overloading the greater than operator to produce a mask."""
+        mask: list[bool] = []
+        if isinstance(rhs, float):
+            for item in self.values:
+                if item > rhs:
+                    mask.append(True)
+                else:
+                    mask.append(False)
+        else:
+            assert len(self.values) == len(rhs.values)
+            i: int = 0
+            while i < len(rhs.values):
+                if self.values[i] > rhs.values[i]:
+                    mask.append(True)
+                else:
+                    mask.append(False)
+                i += 1
+        return mask
+
+    def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Getitem."""
+        if isinstance(rhs, int):
+            return self.values[rhs]
+        else:
+            assert len(self.values) == len(rhs)
+            xs: list[float] = []
+            for i in range(len(rhs)):
+                if rhs[i]:
+                    xs.append(self.values[i])
+            return Simpy(xs)
